9389a4eb05a0311ab63e756eb7053cc22c5d8cb1
omansh <omaansh@gmail.com>
2024-04-07 20:07:35 +0530

    feat: my audio input

---
+++ files.py
@@ -0,0 +1,10 @@
+import os
+
+# Specify the directory
+directory = r'O:\Programming\ML\audio\MossFormer2\MossFormer2_standalone\test_samples\my'
+
+# Get a list of all files in the directory
+file_names = os.listdir(directory)
+
+# Print the list of file names
+print(file_names)
